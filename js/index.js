f6d2ace83a82c09309a4680a9d81e21d89748557
mgomri <mel77gomri@gmail.com>
2020-02-11 01:48:39 -0500

    final commit

--- js/index.js
+++ js/index.js
@@ -16,38 +16,65 @@ const siteContent = {
   },
 
   "main-content": {
-    "features-h4":"Features",
+    "features-h4": "Features",
     "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
+    "about-h4": "About",
     "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
+    "services-h4": "Services",
     "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
+    "product-h4": "Product",
     "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
+    "vision-h4": "Vision",
     "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
 
   "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
+    "contact-h4": "Contact",
+    "address": "123 Way 456 Street Somewhere, USA",
+    "phone": "1 (888) 888-8888",
+    "email": "sales@greatidea.io",
   },
 
 
   "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
+    "copyright": "Copyright Great Idea! 2018"
   },
 
 };
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
-const link = document.getElementById
+const links = document.querySelectorAll('nav a');
+links[0].innerHTML = siteContent['nav']['nav-item-1'];
+links[1].innerHTML = siteContent['nav']['nav-item-2'];
+links[2].innerHTML = siteContent['nav']['nav-item-3'];
+links[3].innerHTML = siteContent['nav']['nav-item-4'];
+links[4].innerHTML = siteContent['nav']['nav-item-5'];
+links[5].innerHTML = siteContent['nav']['nav-item-6'];
+
+const navigation = document.querySelector('nav');
+
+const last = document.createElement('a');
+last.textContent = "last";
+last.style.color = 'red';
+
+navigation.appendChild(last);
+
+const first = document.createElement('a');
+first.textContent = "first";
+navigation.prepend(first);
+first.style.color = "red";
+
+
+
+// document.nav.appendChild(newLinkOne);
+
+links.forEach(link => {
+  link.style.color = "green";
+});
 
 const codeSnippet = document.getElementById('cta-img');
 // codeSnippet.setAttribute('src', siteContent["cta"]["img-src"]);
@@ -85,6 +112,15 @@ footers.textContent = siteContent["footer"]["copyright"];
 
 
 
-
-
-
+//stretch goal.
+const btn = document.createElement('button');
+btn.textContent = "RED BACKGROUND";
+document.body.appendChild(btn);
+btn.style.backgroundColor = 'RED';
+btn.style.display = "block";
+btn.style.margin = " 2rem auto";
+btn.style.height = "3rem";
+// btn.style.width = "10rem";
+btn.addEventListener("click", function () {
+  document.querySelector('button').style.backgroundColor = 'red'
+});
